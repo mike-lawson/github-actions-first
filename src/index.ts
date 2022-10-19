5e9958296fb6974179ec3ebca5d3bcba0a78973f
Mike Lawson <mike@mjlawson.net>
2022-10-19 10:07:56 -0400

    Changes for examples sake

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import fs from "fs/promises";
 const run = async () => {
   try {
     await fs.access(`${__dirname}/github-actions-second/README.md`);
-    console.log("Good");
+    console.log("Good job!");
   } catch (ex) {
     console.log("Bad");
   }
