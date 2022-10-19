c53fdb1d9fbc0aebc332042c0fefda751ab221a4
Mike Lawson <mike@mjlawson.net>
2022-10-19 11:14:32 -0400

    Changes to release

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import fs from "fs/promises";
 const run = async () => {
   try {
     await fs.access(`${__dirname}/github-actions-second/README.md`);
-    console.log("Good job!");
+    console.log("Good!");
   } catch (ex) {
     console.log("Bad job!");
   }
